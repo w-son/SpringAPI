ac33c59aff5a13efc53a930da0c0a578782c7c49
w-son <williamson@naver.com>
2020-05-05 21:51:32 +0900

    ModelMapper, Mockito 대체

--- src/main/java/com/son/SpringAPI/events/EventController.java
+++ src/main/java/com/son/SpringAPI/events/EventController.java
@@ -1,6 +1,7 @@
 package com.son.SpringAPI.events;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,9 +19,15 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 public class EventController {
 
     private final EventRepository eventRepository;
+    private final ModelMapper modelMapper;
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        /*
+         Request의 원하는 정보만 받아서 쓰기 위해 Dto 클래스를 생성했다
+         받아온 정보를 쉽게 매핑하기 위해 ModelMapper 라이브러리를 받아 온 후에 빈에 등록하여 사용하였다
+         */
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = eventRepository.save(event);
         // 아래의 redirectUri를 생성해서 리턴해준다
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
--- src/main/java/com/son/SpringAPI/events/EventDto.java
+++ src/main/java/com/son/SpringAPI/events/EventDto.java
@@ -1,20 +1,17 @@
 package com.son.SpringAPI.events;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
 @Builder
-@Getter @Setter
-@AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode(of = "id")
-@Entity
-public class Event {
-
-    @Id @GeneratedValue
-    private Integer id;
+@AllArgsConstructor
+public class EventDto {
 
     private String name;
     private String description;
@@ -26,10 +23,5 @@ public class Event {
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-
-    @Enumerated(EnumType.STRING)
-    private EventStatus eventStatus = EventStatus.DRAFT;
 
 }
