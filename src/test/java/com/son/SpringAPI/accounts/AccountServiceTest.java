d0c9d893afd09bb785cf943d530881facd705182
w-son <williamson@naver.com>
2020-05-10 15:36:48 +0900

    AccountServiceTest Rule 활용

--- src/test/java/com/son/SpringAPI/accounts/AccountServiceTest.java
+++ src/test/java/com/son/SpringAPI/accounts/AccountServiceTest.java
@@ -1,11 +1,15 @@
 package com.son.SpringAPI.accounts;
 
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -19,6 +23,9 @@ import static org.junit.Assert.*;
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Autowired
     AccountService accountService;
@@ -44,7 +51,17 @@ public class AccountServiceTest {
 
         // Then
         assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
 
+    @Test
+    public void failFindByUsername() throws Exception {
+        // Expected
+        String username = "random@naver.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
     }
 
 }
